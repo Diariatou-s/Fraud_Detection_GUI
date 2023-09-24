d0d7e361c3ad95b5a07bbba3b810e5dd277d45d6
Diariatou Sylla <diariatou.sylla@myriadgroup.com>
2023-09-24 23:05:22 +0000

    Adding MongoDB

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    solde: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
